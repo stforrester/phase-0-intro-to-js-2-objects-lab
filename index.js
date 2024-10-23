a63e87f171dbc895fed053ffac9047504a9f94b6
Stuart Forrester <stforrester@gmail.com>
2024-10-23 16:32:00 -0500

    complete the lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+
+/*const employeesFile = {};
+employeesFile.employee;*/
+
+const employee = {
+    name: "BigXThaPlug",
+    streetAddress: "3314 Drexel Dr"
+};
+
+/*function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee };
+    
+    newEmployee[key] = value;
+
+    return newEmployee;
+};*/
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+
